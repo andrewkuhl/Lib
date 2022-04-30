797a9eef694288e7743d58e584bc70a50245bcd5
andrewkuhl <andrewkuhlz@gmail.com>
2022-04-30 08:38:37 -0700

    moved lib to DFA file

---
+++ DFA.h
@@ -0,0 +1,41 @@
+#ifndef __DFA_H__
+#define __DFA_H__
+
+//knuth morris pratt algo dna
+//but first need dfa class
+//second need product construction of dfa
+//then need nfa
+//convert nfa to dfa
+
+#include <map>
+#include "states.h"
+#include "alph.h"
+#include "trans.h"
+
+#define DEFAULT 0       //DEFAULT 0
+template <class T> class DFA               //DFA CLASS
+{
+    private:
+    public:
+    States<T> * states;       //STATES OBJ
+    Alphabet<T> * alphabet;      //ALPHABET OBJ
+    Transitions<T> * transitions;     //TRANSITIONS OBJ
+
+    DFA()       
+    {
+        states = new States<T>();        //INIT STATES
+        alphabet = new Alphabet<T>();       //INIT ALPHABET
+        transitions = new Transitions<T>();      //INIT TRANSITIONS
+    }
+    ~DFA()
+    {
+        delete states;      //DELETE STATES
+        delete alphabet;        //DELETE ALPHABET
+        delete transitions;         //DELETE TRANSITIONS
+    }
+};
+
+
+
+
+#endif
--- lib.h
+++ lib.h
@@ -1,41 +1,6 @@
 #ifndef __LIB_H__
 #define __LIB_H__
 
-//knuth morris pratt algo dna
-//but first need dfa class
-//second need product construction of dfa
-//then need nfa
-//convert nfa to dfa
-
-#include <map>
-#include "states.h"
-#include "alph.h"
-#include "trans.h"
-
-#define DEFAULT 0       //DEFAULT 0
-template <class T> class DFA               //DFA CLASS
-{
-    private:
-    public:
-    States<T> * states;       //STATES OBJ
-    Alphabet<T> * alphabet;      //ALPHABET OBJ
-    Transitions<T> * transitions;     //TRANSITIONS OBJ
-
-    DFA()       
-    {
-        states = new States<T>();        //INIT STATES
-        alphabet = new Alphabet<T>();       //INIT ALPHABET
-        transitions = new Transitions<T>();      //INIT TRANSITIONS
-    }
-    ~DFA()
-    {
-        delete states;      //DELETE STATES
-        delete alphabet;        //DELETE ALPHABET
-        delete transitions;         //DELETE TRANSITIONS
-    }
-};
-
-
-
+#include "DFA.h"
 
 #endif
